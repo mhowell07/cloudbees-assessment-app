ac7f9b2d5b97b46c0c3e4c29552b91a9b30d89cf
Matt Howell <matthew.howell07@gmail.com>
2025-06-23 18:22:21 -0500

    create users route handler

---
+++ app/api/users/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server';
+
+import { Octokit } from 'octokit';
+
+
+/***
+ *  HTTP methods are not cached by default. This allows us to cache the response of the API
+ * // so we don't need to reach out again until we rebuild the application.
+ ***/
+export const dynamic = 'force-static'
+
+/*** 
+ * Define the GET method response
+ * Documentation Link: https://docs.github.com/en/rest/users/users?apiVersion=2022-11-28#list-users
+ ***/
+const octokit = new Octokit();
+
+export async function GET() {
+
+  try {
+    const response = await octokit.request('/users');
+    const users = await response;
+    return NextResponse.json(users.data);
+  } catch (error) {
+    console.error('Error fetching Github users:', error);
+    return NextResponse.json({ error: 'Failed to fetch users' }, { status: 500 })
+  }
+}
