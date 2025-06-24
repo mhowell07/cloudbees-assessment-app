b6514653326d43c6f778b438f29e5717cefc67ef
Matt Howell <matthew.howell07@gmail.com>
2025-06-24 12:50:37 -0500

    starting to build out user profile

--- app/user/[username]/page.tsx
+++ app/user/[username]/page.tsx
@@ -1,4 +1,5 @@
 import { GithubUserDetail } from "@/types/github";
+import Image from "next/image";
 
 
 type UserPageProps = {
@@ -27,7 +28,17 @@ const UserPage = async ({ params }: UserPageProps) => {
 
 
   return (
-    <></>
+    <div className="w-full py-20 px-4 md:px-8 lg:px-16">
+        <div className="text-center">
+            <div className="flex justify-center mb-8">
+                <Image src={user.avatar_url} width={150} height={150} alt={`${user.login}'s Avatar Image`} className="rounded-full" />
+            </div>
+            <h1 className="font-normal text-gray-900 text-4xl md:text-7xl leading-none mb-8">
+                {user.name}
+            </h1>
+            <h6 className="font-medium text-gray-600 text-lg md:text-2xl uppercase mb-8">@{user.login}</h6>
+        </div>
+    </div>
   )
 }
 
