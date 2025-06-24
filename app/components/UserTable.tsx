4bf1459372b6901c8dd3827693ecd88ed01c35b0
Matt Howell <matthew.howell07@gmail.com>
2025-06-24 12:01:32 -0500

    add avatar images to UserTable

--- app/components/UserTable.tsx
+++ app/components/UserTable.tsx
@@ -2,6 +2,7 @@ import { GithubUser } from '@/types/github';
 import Link from 'next/link';
 import { FaGithubSquare } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa6";
+import Image from 'next/image'
 
 type UserTbableProps = {
   users: GithubUser[];
@@ -22,11 +23,17 @@ const UserTable = ({ users }: UserTbableProps) => {
       <tbody>
         {users.map(user => (
           <tr key={user.id} className="odd:bg-white even:bg-gray-100 hover:bg-gray-100 dark:odd:bg-neutral-800 dark:even:bg-neutral-700 dark:hover:bg-neutral-700">
-            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">John Brown</td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200"><Image src={user.avatar_url} width={50} height={50} alt={`${user.login}'s Avatar Image`} className="rounded-full" /></td>
             <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">{user.login}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200"><Link href={`/user/${user.login}`} className=" flex items-center gap-1">View <FaArrowRight size={12} className="text-gray-800" /></Link></td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
+              <div className="group w-fit">
+                <Link href={`/user/${user.login}`} className="flex items-center gap-1 dark:text-neutral-200 group-hover:text-blue-700 w-fit">View <FaArrowRight size={12} className="text-gray-800 group-hover:text-blue-700 -rotate-45 group-hover:rotate-0 group-hover:trasition-all transition-all" /></Link>
+              </div>
+            </td>
             <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
-              <a href={user.html_url} className="flex justify-end"><FaGithubSquare size={20} className="hover:cursor-pointer"/></a>
+              <div className="flex justify-end">
+                <a href={user.html_url} className="w-fit hover:text-blue-700 trasition-all hover:transition-all"><FaGithubSquare size={25} className="hover:cursor-pointer"/></a>
+              </div>
             </td>
           </tr>
         ))}
