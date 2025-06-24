b889c2813adfe63af1bf81a56f36107405ed194a
Matt Howell <matthew.howell07@gmail.com>
2025-06-23 19:31:45 -0500

    add type for GithubUserDetail

--- app/types/github.ts
+++ app/types/github.ts
@@ -4,3 +4,21 @@ export interface GithubUser {
   avatar_url: string;
   html_url: string;
 }
+
+export interface GithubUserDetail {
+  login: string;
+  id: number;
+  avatar_url: string;
+  html_url: string;
+  name: string | null;
+  company: string | null;
+  location: string | null;
+  bio: string | null;
+  twitter_username: string | null;
+  public_repos: number;
+  public_gists: number;
+  followers: number;
+  following: number;
+  created_at: string;
+  updated_at: string;
+}
