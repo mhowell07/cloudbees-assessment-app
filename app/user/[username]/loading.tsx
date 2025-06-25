7c00ee289efd3524e11dd983cbeb85c24ce8b5a6
Matt Howell <matthew.howell07@gmail.com>
2025-06-25 12:44:53 -0500

    add loading boundary to user details page

---
+++ app/user/[username]/loading.tsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+  
+  return (
+    <div className="flex flex-col w-full items-center justify-center h-screen">
+      <p className="text-xl text-gray-600">Loading user data...</p>
+    </div>
+  );
+}
+
+export default Loading;
