7b96aa0242b112f195dbd98d22f283aa415b9b51
Matt Howell <matthew.howell07@gmail.com>
2025-06-25 16:15:27 -0500

    fixing broken config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
